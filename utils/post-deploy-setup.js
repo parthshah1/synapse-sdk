03f9d7a8a5d6b5615edefe03180a95f046065d06
parthshah1 <parthhshah171@gmail.com>
2025-11-05 16:11:42 +0530

    fixing sentry and viem issues, not needed for devnet antithesis

--- utils/post-deploy-setup.js
+++ utils/post-deploy-setup.js
@@ -93,10 +93,10 @@
  */
 
 import { ethers } from 'ethers'
-import { PaymentsService } from '../packages/synapse-sdk/src/payments/service.ts'
-import { SPRegistryService } from '../packages/synapse-sdk/src/sp-registry/service.ts'
-import { CONTRACT_ADDRESSES, RPC_URLS, TIME_CONSTANTS, TOKENS } from '../packages/synapse-sdk/src/utils/constants.ts'
-import { WarmStorageService } from '../packages/synapse-sdk/src/warm-storage/service.ts'
+import { PaymentsService } from '../packages/synapse-sdk/dist/src/payments/service.js'
+import { SPRegistryService } from '../packages/synapse-sdk/dist/src/sp-registry/service.js'
+import { CONTRACT_ADDRESSES, RPC_URLS, TIME_CONSTANTS, TOKENS } from '../packages/synapse-sdk/dist/src/utils/constants.js'
+import { WarmStorageService } from '../packages/synapse-sdk/dist/src/warm-storage/service.js'
 
 // Constants for payment approvals
 const RATE_ALLOWANCE_PER_EPOCH = ethers.parseUnits('0.1', 18) // 0.1 USDFC per epoch
